0c569fe26bcfabb42d900d1b7f1958b32531dbdf
Natalia Ratanova <r-natash3@mail.ru>
2017-01-25 22:49:03 +0300

    Добавлены функции расчета площади кв и прям-ка

--- sandbox/src/main/java/ru/pft/sandbox/FirstProgram.java
+++ sandbox/src/main/java/ru/pft/sandbox/FirstProgram.java
@@ -3,6 +3,27 @@ package ru.pft.sandbox;
 public class FirstProgram
 {
 	public static void main (String[] Args) {
-		System.out.println("Hello, world!!!");
+		hello("world");
+    hello("chel");
+    hello("Natali");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + kv(l));
+
+    double a1 = 2, b1=4;
+    System.out.println("Плащадь прямоугольника со сторонами " + a1 + ", "+ b1 + " = " + pryam(a1,b1));
+
 	}
+
+	public static void hello (String text) {
+	  System.out.println("Hello," + text + "!");
+  }
+
+  public static double kv (double len){
+	  return len*len;
+  }
+
+  public static double pryam (double a, double b) {
+    return a*b;
+  }
 }
