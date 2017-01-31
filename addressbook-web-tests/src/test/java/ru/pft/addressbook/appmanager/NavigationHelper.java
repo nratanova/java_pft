c0ac063838ae859311073825aa589eecaffc3ee7
Natalia Ratanova <r-natash3@mail.ru>
2017-02-01 01:00:44 +0300

    Добавлены вспомогательные классы

---
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Наташа on 01.02.2017.
+ */
+public class NavigationHelper {
+  private FirefoxDriver wd;
+
+  public NavigationHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+}
