981ad950207fd88a2483f676d4f024bab5cf803a
Natalia Ratanova <r-natash3@mail.ru>
2017-02-05 14:48:11 +0300

    Добавлены Equations\EquationsTests

---
+++ sandbox/src/test/java/ru/pft/sandbox/EquationsTests.java
@@ -0,0 +1,28 @@
+package ru.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Наташа on 05.02.2017.
+ */
+public class EquationsTests {
+
+  @Test
+  public void testEq1() {
+    Equations e = new Equations(1, 1, 1);
+    Assert.assertEquals(e.rootNumber(),0);
+  }
+
+  @Test
+  public void testEq2() {
+    Equations e = new Equations(1, 2, 1);
+    Assert.assertEquals(e.rootNumber(),1);
+  }
+
+  @Test
+  public void testEq3() {
+    Equations e = new Equations(1, 5, 6);
+    Assert.assertEquals(e.rootNumber(),2);
+  }
+}
