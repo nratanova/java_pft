33aa6e6b538686f407b49cda8b1f8c4e845656c3
Natalia Ratanova <r-natash3@mail.ru>
2017-02-05 15:23:42 +0300

    Расширены проверки If\Then\Else

--- sandbox/src/main/java/ru/pft/sandbox/Equations.java
+++ sandbox/src/main/java/ru/pft/sandbox/Equations.java
@@ -10,22 +10,34 @@ public class Equations {
   private double c;
   private int n;
 
-  public Equations (double a, double b, double c){
+  public Equations(double a, double b, double c) {
 
     this.a = a;
     this.b = b;
     this.c = c;
 
-    double d=b*b - 4*a*c;
+    double d = b * b - 4 * a * c;
 
-    if (d>0){
-      n=2;
-    } else {
-      if (d==0) {
-        n=1;
+    if (a != 0) {
+
+      if (d > 0) {
+        n = 2;
+
+      } else if (d == 0) {
+        n = 1;
+
       } else {
-        n=0;
+        n = 0;
       }
+
+    } else if (b != 0) {
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+
+    } else {
+      n = -1;
     }
   }
 
--- sandbox/src/test/java/ru/pft/sandbox/EquationsTests.java
+++ sandbox/src/test/java/ru/pft/sandbox/EquationsTests.java
@@ -25,4 +25,22 @@ public class EquationsTests {
     Equations e = new Equations(1, 5, 6);
     Assert.assertEquals(e.rootNumber(),2);
   }
+
+  @Test
+  public void testEq4() {
+    Equations e = new Equations(0, 5, 6);
+    Assert.assertEquals(e.rootNumber(),1);
+  }
+
+  @Test
+  public void testEq5() {
+    Equations e = new Equations(0, 0, 6);
+    Assert.assertEquals(e.rootNumber(),0);
+  }
+
+  @Test
+  public void testEq6() {
+    Equations e = new Equations(0, 0, 0);
+    Assert.assertEquals(e.rootNumber(),-1);
+  }
 }
