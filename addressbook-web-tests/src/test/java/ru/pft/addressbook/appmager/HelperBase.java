3d0c616bfd2956d8d68545f920dc61893ff97cce
Natalia Ratanova <r-natash3@mail.ru>
2017-02-06 23:59:47 +0300

    Тесты в разных браузерах

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/appmager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/appmager/HelperBase.java
@@ -2,15 +2,15 @@ package ru.pft.addressbook.appmager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 
 /**
  * Created by Наташа on 03.02.2017.
  */
 public class HelperBase {
-  protected FirefoxDriver wd;
+  protected WebDriver wd;
 
-  public HelperBase(FirefoxDriver wd) {
+  public HelperBase(WebDriver wd) {
     this.wd = wd;
   }
 
