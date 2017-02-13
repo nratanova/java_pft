82d1f77b66f2c48c774b65ebc41ffe1588b0e686
Natalia Ratanova <r-natash3@mail.ru>
2017-02-13 23:49:28 +0300

    Ввод одинаковых значений при изменении группы

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("TestMod", "TestMod2", "TestMod3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("Test", "Test2", "Test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
   }
