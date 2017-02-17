ac65a68524f764860fc51b03d980a3deb33c219c
Natalia Ratanova <r-natash3@mail.ru>
2017-02-17 23:27:32 +0300

    Оптимизация переходов между страницами

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/appmager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/appmager/NavigationHelper.java
@@ -7,13 +7,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Наташа on 02.02.2017.
  */
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
@@ -22,6 +27,10 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoHomePage() {
+
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }
