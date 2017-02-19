abd13c03145320c909155cfaf1a0094867a119fc
Natalia Ratanova <r-natash3@mail.ru>
2017-02-19 15:35:09 +0300

    Добавлены предусловия для работы с группами

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupDelTests.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupDelTests.java
@@ -1,12 +1,16 @@
 package ru.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.pft.addressbook.model.GroupData;
 
 public class GroupDelTests extends TestBase {
 
     @Test
     public void testGroupDel() {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
