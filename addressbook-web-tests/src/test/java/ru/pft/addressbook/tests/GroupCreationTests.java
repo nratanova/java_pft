814b661bfd9eb1b651663bb02eb0374c04d537c4
Natalia Ratanova <r-natash3@mail.ru>
2017-03-05 16:01:15 +0300

    Цикл вычисления макс ID группы заменен на лямбда функцию

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,19 +14,18 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("Test1", null, null);
+    GroupData group = new GroupData("Test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1); //Проверка, что после создания группы кол-во групп увелисилось на 1
 
 //Вычисление максимальногоо ID (ID новой группы)
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-      group.setId(max);
-    }
+
+    /*Превращение списка в поток stream(), по потоку пробегается лямбда функция - сравниватель,
+    котрая находит максимальный элемент сравнивая объекты типы GroupData по идентификатору
+    на выходе будет группа с максимальным идентификатором, достаем это id - get().getId()*/
+
+    group.setId(after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));//Сравнение множеств групп
   }
