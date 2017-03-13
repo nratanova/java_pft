5a8e0e39894151c09450751419c27e699f86ecf7
Natalia Ratanova <r-natash3@mail.ru>
2017-03-14 00:50:40 +0300

    Добавлен итератор для теста по созданию групп

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupCreationTests.java
@@ -1,29 +1,45 @@
 package ru.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.pft.addressbook.model.GroupData;
 import ru.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("Group2");//Создается объект с именем Group2
-    app.group().create(group);
-    assertThat(app.group().count(), equalTo(before.size() + 1)); //Хэширование, предварительная проверка,
-    // при помощи более быстрого способа
-    Groups after = app.group().all();
-    assertThat(after, equalTo
-            (before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
-    //CoreMatchers.equalTo - проверялка, параметр - то, что надо проверить
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();//делаем список массивов объектов
+    list.add(new Object[] {new GroupData().withName("Test")
+            .withHeader("Header1").withFooter("Footer1")});//Заполнение списка тестовыми данными
+    list.add(new Object[] {new GroupData().withName("Test2")
+            .withHeader("Header2").withFooter("Footer2")});//Заполнение списка тестовыми данными
+    list.add(new Object[] {new GroupData().withName("Test3")
+            .withHeader("Header3").withFooter("Footer3")});//Заполнение списка тестовыми данными
+    return list.iterator();
   }
 
-  @Test
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+      app.group().create(group);
+      assertThat(app.group().count(), equalTo(before.size() + 1)); //Хэширование, предварительная проверка,
+      // при помощи более быстрого способа
+      Groups after = app.group().all();
+      assertThat(after, equalTo
+              (before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+      //CoreMatchers.equalTo - проверялка, параметр - то, что надо проверить
+  }
+
+  @Test (enabled = false)
   //Негативный тест, проверяющий, что группа с таким названием не создается
   public void testBadGroupCreation() {
     app.goTo().groupPage();
