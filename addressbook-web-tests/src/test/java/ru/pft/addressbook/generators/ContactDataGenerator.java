ec670c6acc8bd6530f81360254ef8dc57aec5eaf
Natalia Ratanova <r-natash3@mail.ru>
2017-03-16 00:41:06 +0300

    Добавлен генератор данных контактов и загрузка из файла

---
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,97 @@
+package ru.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
+import ru.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Наташа on 15.03.2017.
+ */
+public class ContactDataGenerator {
+
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
+  public static void main(String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    if (format.equals("csv")) {
+      saveAsCSV(contacts, new File(file));
+    } else if (format.equals("xml")) {
+      saveAsXML(contacts, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJSON(contacts, new File(file));
+    } else {
+      System.out.println("Unrecognized format" + format);
+    }
+  }
+
+  private void saveAsJSON(List<ContactData> contacts, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();//Пропуск
+    //всех полей, которые не поомечены анотацией @Expose (для gson)
+    String json = gson.toJson(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
+
+  private void saveAsXML(List<ContactData> contacts, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    String xml = xstream.toXML(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+  }
+
+  //Запись в файл
+  private void saveAsCSV(List<ContactData> contacts, File file) throws IOException {
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts) {
+      writer.write(String.format("%s;%s;%s;%s;%s;%s\n", contact.getFirstName(), contact.getLastName(), contact.getMobPhone(),
+              contact.getGroup(), contact.getEmail(), contact.getPhoto().getAbsolutePath()));
+    }
+    writer.close();
+  }
+
+  private List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData()
+              .withFirstName(String.format("Nat %s", i))
+              .withLastName(String.format("Rat %s", i))
+              .withGroup(String.format("Test", i))
+              .withEmail(String.format("Nat%s@mail.ru", i))
+              .withMobPhone(String.format("8905909909%s", i)));
+    }
+    return contacts;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,63 @@
 package ru.pft.addressbook.tests;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.pft.addressbook.model.ContactData;
 import ru.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTests extends TestBase {
-  @Test
-  public void testContactCreation() {
+
+  @DataProvider
+  public Iterator<Object[]> validContactsXML() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((g)-> new Object[] {g} ).collect(Collectors.toList()).iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validContactsJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+    return contacts.stream().map((g)-> new Object[] {g} ).collect(Collectors.toList()).iterator();
+  }
+
+  @Test (dataProvider = "validContactsJson")
+  public void testContactCreation(ContactData contact) {
     app.goTo().gotoHomePage();
     Contacts before = app.contact().all();
-    File photo = new File ("src/test/resources/panda.jpg");
-    ContactData contact = new ContactData().withFirstName("Nat").withLastName("Rat").withHomePhone("79078965454").
-            withMobPhone("11111111111").withWorkPhone("79064531223").withEmail("qwert3@gmail.com").withGroup("Test").withPhoto(photo);
+    //File photo = new File ("src/test/resources/panda.jpg");
+    //ContactData contact = new ContactData().withFirstName("Nat").withLastName("Rat").withHomePhone("79078965454").
+      //      withMobPhone("11111111111").withWorkPhone("79064531223").withEmail("qwert3@gmail.com").withGroup("Test").withPhoto(photo);
     app.contact().create((contact), true);
     assertEquals(app.contact().count(), before.size() + 1); //Хэширование
     Contacts after = app.contact().all();
