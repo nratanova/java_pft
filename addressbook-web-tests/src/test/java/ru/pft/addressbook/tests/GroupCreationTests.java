228b11b4728fe95153638e1f549efc08b9e5f480
nratanova <r-natash3@mail.ru>
2017-03-22 22:49:28 +0300

    Добавлено протоколирование

--- addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.pft.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.pft.addressbook.model.GroupData;
--- addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,25 @@
 package ru.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.pft.addressbook.appmager.ApplicationManager;
+
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Наташа on 02.02.2017.
  */
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   public static final ApplicationManager app =
           new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -18,9 +29,18 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite (alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
 
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test " + m.getName());
+  }
 }
