408b8da018f7e1d5cde7e4ed9043ebdeea9ac272
nratanova <r-natash3@mail.ru>
2017-03-22 23:50:41 +0300

    Добавлен тест, проверяющий соединение с БД

---
+++ addressbook-web-tests/src/test/java/ru/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,43 @@
+package ru.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.pft.addressbook.model.GroupData;
+import ru.pft.addressbook.model.Groups;
+
+import java.sql.*;
+
+/**
+ * Created by Natalya on 22.03.2017.
+ */
+public class DbConnectionTest {
+
+    @Test
+    public void testDbConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&" +
+                    "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Moscow");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select group_id, group_name, group_header, group_footer from group_list");
+            Groups groups = new Groups();
+            while (rs.next()) {
+                groups.add(new GroupData().withId(rs.getInt("group_id"))
+                        .withName(rs.getString("group_name"))
+                        .withHeader(rs.getString("group_header"))
+                        .withFooter(rs.getString("group_footer")));
+            }
+            rs.close(); //закрыть запрос, больше не надо читать данные
+            st.close(); //закрыть, больше не надо выполнять запросы
+            conn.close(); //закрыть соединение с бд
+
+            System.out.println(groups);
+
+            // Do something with the Connection
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+}
